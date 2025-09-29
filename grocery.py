91a9c3b7a2542be48d5748e884f53ffb02a45fa8
Ali Fadel <fadelali0123@gmail.com>
2025-09-29 23:22:10 +0300

    addding functionalities

--- grocery.py
+++ grocery.py
@@ -7,7 +7,22 @@ while True:
     parts = item.split()
     product = parts[0]
     quantity =int(part[1]) if len(parts) >1 else 1
-if product in groceries :
-    cart[product] = cart.get(product,0)+ quantity
-else:
-    print("sorry,we dont have that item.")
+    if product in groceries :
+      if product in cart:
+         cart[item] += quantity
+      else:
+         cart[item] = quantity
+    else:
+      print("sorry,we dont have that item.")
+    total= 0
+    for item ,qty in cart.items():
+       price=groceries[item]
+       if item =="milk" and qty > 2:
+          price -= 1
+       total += price *qty
+    print(" your cart",cart)
+    print(f"Total =${total}")
+    if total >10:
+       print("you spent a lot")
+    else:
+       print("you spent a little!")
